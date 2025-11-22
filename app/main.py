f7a0eb42978240466bd2ecc1dd2989a56e22ff75
Samuel-Tefera <samuelteferab@gmail.com>
2025-11-22 10:21:42 +0300

    add room route to main app

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from app.routes import hotel_manager, hotel
+from app.routes import hotel_manager, hotel, room
 
 app = FastAPI(title='MyStay API')
 
 app.include_router(hotel_manager.router, prefix='/api')
 app.include_router(hotel.router, prefix='/api')
+app.include_router(room.router, prefix='/api')
