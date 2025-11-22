7c25f5d065bc7f376ac505179b6fc3380c202095
Samuel-Tefera <samuelteferab@gmail.com>
2025-11-22 10:20:22 +0300

    create crud functions for room

---
+++ app/crud/room.py
@@ -0,0 +1,33 @@
+from sqlalchemy import and_
+from sqlalchemy.orm import Session
+
+from app.models.room import Room
+from app.schemas.room import RoomBase
+
+
+# Creating new room for Hotel
+def create_new_room(hotel_id, new_room: RoomBase, db: Session):
+  db_room = Room(
+    hotel_id = hotel_id,
+    room_number = new_room.room_number,
+    room_type = new_room.room_type,
+    price_per_night = new_room.price_per_night,
+    description = new_room.description,
+    bed_type = new_room.bed_type,
+    image_url = new_room.image_url,
+    available = new_room.available,
+  )
+
+  db.add(db_room)
+  db.commit()
+  db.refresh(db_room)
+
+  return db_room
+
+# Get all rooms for one hotel
+def get_all_rooms(hotel_id: int, db: Session):
+  return db.query(Room).filter(Room.hotel_id == hotel_id)
+
+# get one room
+def get_one_room(hotel_id: int, room_id: int, db: Session):
+  return db.query(Room).filter(and_(Room.id == room_id, Room.hotel_id == hotel_id)).first()
