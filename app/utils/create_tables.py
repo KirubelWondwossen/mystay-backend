cc6b6ef2fcafcffa7a39f851fbb59d71e7e17615
Samuel-Tefera <samuelteferab@gmail.com>
2025-12-01 11:38:06 +0300

    move create table code to utils

--- app/utils/create_tables.py
+++ app/utils/create_tables.py
@@ -1,5 +1,7 @@
 from app.database import Base, engine
-from app.models.users import HotelManager
+from app.models.users import HotelManager, Guest
+from app.models.hotel import Hotel
+from app.models.room import Room
 
 print("Creating tables in database...")
 Base.metadata.create_all(bind=engine)
