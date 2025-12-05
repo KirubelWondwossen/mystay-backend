f5a0bf1d160d242d0813939a08501de7ebf4d77c
Samuel-Tefera <samuelteferab@gmail.com>
2025-12-05 22:15:36 +0300

    implement login and update password route

--- app/routes/hotel_manager.py
+++ app/routes/hotel_manager.py
@@ -1,30 +1,75 @@
 from fastapi import APIRouter, Depends, HTTPException
 
 from sqlalchemy.orm import Session
+from app.core.dependencies import require_hotel_manager
+from app.core.security import create_manager_token, hash_password, verify_password
 from app.crud.users import create_hotel_manager, get_all_hotel_managers, get_hotel_manager
 from app.database import get_db
+from app.models.users import HotelManager
 
-from app.schemas.users import HotelManagerCreate, HotelManagerDisplay
+from app.schemas.users import HotelManagerCreate, HotelManagerDisplay, HotelManagerLogin, HotelManagerPasswordUpdate
 
 
 router = APIRouter(prefix='/hotelmanager', tags=['HotelManager'])
 
-# Create Manager
-@router.post('/', response_model=HotelManagerDisplay)
-def create_manager(new_manager: HotelManagerCreate, db: Session = Depends(get_db)):
-  return create_hotel_manager(db, new_manager)
+# Handle login
+@router.post('/login')
+def hotel_manager_login(credentials: HotelManagerLogin, db: Session = Depends(get_db)):
+  manager = db.query(HotelManager).filter(HotelManager.email == credentials.email).first()
 
-# Get all managers
-@router.get('/', response_model=list[HotelManagerDisplay])
-def get_managers(db: Session = Depends(get_db)):
-  return get_all_hotel_managers(db)
+  if not manager or not verify_password(credentials.password, manager.password):
+    raise HTTPException(status_code=401, detail='Invalid credentials')
 
-# Get one manager by ID
-@router.get('/{manager_id}', response_model=HotelManagerDisplay)
-def get_manager(manager_id: int, db: Session = Depends(get_db)):
-  db_manager = get_hotel_manager(db, manager_id)
+  token = create_manager_token(manager)
 
-  if not db_manager:
-    raise HTTPException(status_code=404, detail='Manager not found')
+  return {
+    "access_token": token,
+    "token_type": "bearer",
+    "manager_id": manager.id,
+    "manager_name": manager.name,
+    "manager_email": manager.email,
+  }
 
-  return db_manager
+
+# Handle update password
+@router.patch('/{manager_id}/update-password')
+def update_password(
+  manager_id: int,
+  credentials: HotelManagerPasswordUpdate,
+  db: Session = Depends(get_db),
+  token_data = Depends(require_hotel_manager)):
+
+  if int(token_data['sub']) != manager_id:
+        raise HTTPException(status_code=403, detail='Not allowed')
+
+  manager = db.query(HotelManager).filter(HotelManager.id == manager_id).first()
+
+  if not manager or not verify_password(credentials.current_password, manager.password):
+    raise HTTPException(status_code=401, detail='Invalid credentials')
+
+  manager.password = hash_password(credentials.new_password)
+  db.commit()
+  db.refresh(manager)
+
+  return {'message': 'Password Successfully Updated'}
+
+
+# # Create Manager
+# @router.post('/', response_model=HotelManagerDisplay)
+# def create_manager(new_manager: HotelManagerCreate, db: Session = Depends(get_db)):
+#   return create_hotel_manager(db, new_manager)
+
+# # Get all managers
+# @router.get('/', response_model=list[HotelManagerDisplay])
+# def get_managers(db: Session = Depends(get_db)):
+#   return get_all_hotel_managers(db)
+
+# # Get one manager by ID
+# @router.get('/{manager_id}', response_model=HotelManagerDisplay)
+# def get_manager(manager_id: int, db: Session = Depends(get_db)):
+#   db_manager = get_hotel_manager(db, manager_id)
+
+#   if not db_manager:
+#     raise HTTPException(status_code=404, detail='Manager not found')
+
+#   return db_manager
