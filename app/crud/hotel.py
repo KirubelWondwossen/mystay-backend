4692eef359f6e75063ebdd7052ff151dd67e4a26
Samuel-Tefera <samuelteferab@gmail.com>
2025-12-15 11:39:45 +0300

    modify crud on hotels

--- app/crud/hotel.py
+++ app/crud/hotel.py
@@ -1,11 +1,16 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app.models.hotel import Hotel, HotelApplication
 
-from app.schemas.hotel import HotelApplicationCreate, HotelCreate
+from app.schemas.hotel import HotelApplicationCreate, HotelUpdate
 
 
 # Creating new hotel application
-def create_new_hotel_application(db: Session, new_hotel_application: HotelApplicationCreate):
+def create_new_hotel_application(
+  db: Session,
+  new_hotel_application: HotelApplicationCreate
+  ):
+
   db_hotel_application = HotelApplication(
     manager_name = new_hotel_application.manager_name,
     manager_email = new_hotel_application.manager_email,
@@ -29,30 +34,19 @@ def get_all_hotel_applications(db : Session):
   return db.query(HotelApplication).all()
 
 # Get hotel application detail
-def get_hotel_application_detail(db: Session, application_id: int):
+def get_hotel_application_detail(
+  db: Session,
+  application_id: int):
+
   return db.query(HotelApplication).filter(HotelApplication.id == application_id).first()
-
-# Creating new Hotel
-def create_new_hotel(db: Session, new_hotel: HotelCreate):
-  db_hotel = Hotel(
-    manager_id = new_hotel.manager_id,
-    name = new_hotel.name,
-    description = new_hotel.description,
-    address = new_hotel.address,
-    rating = new_hotel.rating,
-    exact_location = new_hotel.exact_location.dict(),
-  )
-
-  db.add(db_hotel)
-  db.commit()
-  db.refresh(db_hotel)
-
-  return db_hotel
 
 # Get all Hotels
 def get_all_hotels(db: Session):
   return db.query(Hotel).all()
 
 # Get Hotel
-def get_one_hotel(db: Session, hotel_id: int):
+def get_one_hotel(
+  db: Session,
+  hotel_id: int):
+
   return db.query(Hotel).filter(Hotel.id == hotel_id).first()
